b1ec5d1caaf4aad496c947384c0b67dd2cca6d95
João Fraga <joaoluisfreirefraga@gmail.com>
2020-01-04 00:59:21 +0000

    Implement functionality

--- src/estou-a-ver.py
+++ src/estou-a-ver.py
@@ -15,6 +15,12 @@ DATABASE_NAME = "database.db"
 
 DAEMON_SLEEP_TIME = 300
 
+DATABASE_INFO = None
+
+# ------------------------------------------------------------------------------
+# Data Structures
+
+
 # ------------------------------------------------------------------------------
 # Strings
 
@@ -65,51 +71,76 @@ def get_arguments():
 
 def SHA256(filename):
     '''This function returns the SHA256 of the filename'''
-    output = subprocess.Popen(
+    output = subprocess.run(
         ['openssl', 'dgst', '-sha256', filename],
         stdout=subprocess.PIPE,
         universal_newlines=True)
-    return output.stdout.read()
+    return output
 
 
-def AES_128_CBC(filename, hex_key, iv):
-    '''This function encrypts the filename with AES-128-CBC'''
-    subprocess.Popen(
-        ['openssl', 'enc', '-aes-128-cbc', '-K',
-            hex_key, '-in', filename, '-out', filename, '-iv', iv],
+def get_salt_key_iv(password):
+    '''This function generates a salt, key, iv pair'''
+    output = subprocess.run(
+        ['openssl', 'enc', '-aes-256-cbc', '-k', password, '-P'],
         stdout=subprocess.PIPE,
         universal_newlines=True)
+    return output
 
 
-def create_database(args):
+def encrypt_AES_128_CBC(filename, content, salt, key, iv):
+    '''This function encrypts with AES-128-CBC'''
+    output = subprocess.run(
+        ['openssl', 'enc', '-aes-128-cbc', '-K',
+            key, '-out', filename, '-iv', iv, '-S', salt],
+        input=content,
+        stdout=subprocess.PIPE,
+        universal_newlines=True)
+    return output
+
+
+def decrypt_AES_128_CBC(filename):
+    '''This function decrypts with AES-128-CBC'''
+    salt, key, iv = DATABASE_INFO
+    output = subprocess.run(
+        ['openssl', 'enc', '-aes-128-cbc', '-d', '-K',
+            key, '-in', filename, '-iv', iv, '-S', salt],
+        stdout=subprocess.PIPE)
+    return output
+
+
+def create_database(args, password):
     '''This function calculates hashes SHA256 for all files inside a directory and saves them to the database'''
-    # Open database file
-    db = open(DATABASE_NAME, "w")
+    # Access global variable
+    global DATABASE_INFO
     # Go to directory
     os.chdir(args.directory)
+    # Database data
+    hashes = ''
     # For each file in directory
     for filename in get_files(args.directory):
         # Get hash
         output = SHA256(filename)
-        # Write to database
-        db.write(output)
-    db.close()
+        # Add to list
+        hashes += output.stdout
+    # Get encryption info
+    output = get_salt_key_iv(password)
+    # Parse output
+    output = output.stdout.split('\n')
+    salt = output[0].split('=')[1]
+    key = output[1].split('=')[1]
+    iv = output[2].split('=')[1]
+    # Store info
+    DATABASE_INFO = salt, key, iv
+    # Encrypt the data
+    output = encrypt_AES_128_CBC(DATABASE_NAME, hashes, salt, key, iv)
 
 
-# def directory_monitor():
-#     '''This function checks for changes inside directory'''
-#     # guardar lista de ficheiros na observação atual
-#     current = get_managed_files()
-#     # Ver se algum ficheiro foi adicionado
-#     adicionado = [f for f in current if not f in managed_files]
-#     # Ver se algum ficheiro foi removido
-#     removido = [f for f in managed_files if not f in current]
-#     if adicionado:
-#         print("Adicionado: ", adicionado)
-#     if removido:
-#         print("Removido: ", removido)
-#     managed_files = current
-#     db_write_hashes()
+def read_database(args):
+    # Go to directory
+    os.chdir(args.directory)
+    # Decrypt the data
+    output = decrypt_AES_128_CBC(DATABASE_NAME)
+    return output.stdout.decode('utf-8')
 
 
 def main_daemon(args):
@@ -126,13 +157,17 @@ def main(args):
     print("directory: " + args.directory, "daemon: " +
           str(args.daemon), "remove: " + str(args.remove), sep='\n')
     # TODO: implement functionality
-    create_database(args)
+    password = input("Insert a password for database: ")
+    create_database(args, password)
+    db = read_database(args)
+    print(db)
 
 
 # ------------------------------------------------------------------------------
 # Entry Point
 
 if __name__ == "__main__":
+
     args = get_arguments()
 
     if args.daemon:
